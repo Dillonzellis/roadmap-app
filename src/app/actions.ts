04d4e51e92d0b87f2c4207fe78be7d2f0b634b3c
dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2026-07-03 22:12:41 -0400

    switch to postgres with neon

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,19 +1,19 @@
-"use server";
-
-import { db } from "@/db";
-import { tracks } from "@/db/schema";
-import { slugify } from "@/lib/slugify";
-import { revalidatePath } from "next/cache";
-
-export async function createTrack(formData: FormData) {
-  const formTrackName = formData.get("name");
-
-  if (typeof formTrackName !== "string") {
-    throw new Error("");
-  }
-
-  await db
-    .insert(tracks)
-    .values({ name: formTrackName, slug: slugify(formTrackName) });
-  revalidatePath("/tracks");
-}
+// "use server";
+//
+// import { db } from "@/db";
+// import { tracks } from "@/db/schema";
+// import { slugify } from "@/lib/slugify";
+// import { revalidatePath } from "next/cache";
+//
+// export async function createTrack(formData: FormData) {
+//   const formTrackName = formData.get("name");
+//
+//   if (typeof formTrackName !== "string") {
+//     throw new Error("");
+//   }
+//
+//   await db
+//     .insert(tracks)
+//     .values({ name: formTrackName, slug: slugify(formTrackName) });
+//   revalidatePath("/tracks");
+// }
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,8 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const tracks = sqliteTable("tracks", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  name: text("name").notNull(),
-  slug: text("slug").notNull().unique(),
-  description: text("description"),
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
 });
